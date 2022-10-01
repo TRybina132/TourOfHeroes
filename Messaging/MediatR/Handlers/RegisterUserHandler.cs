07bff0e2d52b255e23d260c5c48f719ed96e57de
= <--add>
2022-10-01 17:25:53 +0300

    resolved

---
+++ Messaging/MediatR/Handlers/RegisterUserHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Messaging.MediatR.Notifications;
+using Service.ServicesAbstractions;
+
+namespace Messaging.MediatR.Handlers
+{
+    internal class RegisterUserHandler : INotificationHandler<UserRegisterNotification>
+    {
+        private readonly IUserService userService;
+
+        public RegisterUserHandler(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task Handle(UserRegisterNotification notification, CancellationToken cancellationToken)
+        {
+            await userService.AddUser(notification.User);
+        }
+    }
+}
---
+++ Messaging/MediatR/Notifications/UserRegisterNotification.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using MediatR;
+
+namespace Messaging.MediatR.Notifications
+{
+    public class UserRegisterNotification : INotification
+    {
+        public User User { get; set; }
+
+        public UserRegisterNotification(User user)
+        {
+            User = user;
+        }
+    }
+}
