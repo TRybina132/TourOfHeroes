7cdf125c730c767326db093288c533de112d38f2
= <--add>
2022-10-02 14:52:32 +0300

    added test config

---
+++ Tests/ApiTests/Factories/TestWebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiTests.Factories
+{
+    public class TestWebAppFactory<TEntryPoint> 
+        : WebApplicationFactory<Program>
+        where TEntryPoint : Program
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                ServiceDescriptor? descriptor = services
+                    .SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<TourContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                //  ᓚᘏᗢ Creating temporary in-memory database
+                services.AddDbContext<TourContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryTourOfHeroes");
+                });
+                
+
+            });
+            //base.ConfigureWebHost(builder);
+        }
+    }
+}
